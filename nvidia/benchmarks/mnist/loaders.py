4844114fc01ab9a264ce5ec598f393b8056b6635
Todd <myoung44@vols.utk.edu>
2018-05-25 16:11:34 -0400

    Adding data loaders

---
+++ nvidia/benchmarks/mnist/loaders.py
@@ -0,0 +1,36 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+class MNISTLoaders:
+    """
+    Data loaders for MNIST.
+    """
+    def __init__(self, batch_size):
+        self.batch_size = batch_size
+
+    def train_loader(self, data_dir, kwargs):
+        loader = DataLoader(
+          datasets.MNIST(
+            data_dir, train=True, download=True,
+            transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])
+          ),
+          batch_size=self.batch_size, shuffle=True, **kwargs
+        )
+
+        return loader
+
+    def test_loader(self, data_dir, kwargs):
+        loader = torch.utils.data.DataLoader(
+          datasets.MNIST(
+            data_dir, train=False, 
+            transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])
+            ),
+            batch_size=self.batch_size, shuffle=True, **kwargs
+          )
---
+++ nvidia/benchmarks/mnist/main.py
@@ -0,0 +1,63 @@
+import torch
+
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+    
+    if batch_idx % args.log_interval == 0:
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).item() # sum up batch loss
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+
+def main():
+    args = parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    torch.manual_seed(args.seed)
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+
+
+if __name__=="__main__":
+    main()
